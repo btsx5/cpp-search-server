9fc4bee5afb4e78f28ba43521c6d48c2a1c22c95
Aleksandr Andrukhovich <alex.andr5x@yandex.ru>
2023-01-09 20:33:19 +0300

    sprint6_final_fix

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -101,7 +101,7 @@ std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_quer
 
     sort(matched_documents.begin(), matched_documents.end(),
          [](const Document& lhs, const Document& rhs) {
-             if (std::abs(lhs.relevance - rhs.relevance) < 1e-6) {
+             if (std::abs(lhs.relevance - rhs.relevance) < EPSILON) {
                  return lhs.rating > rhs.rating;
              } else {
                  return lhs.relevance > rhs.relevance;
